642b47b0143cc398c5d09ad74f3e91594949e35d
Ran Malki <ran.malki2@gmail.com>
2021-10-17 19:47:20 +0300

    commit

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,13 +1,33 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+async function create(req, res) {
+    const user = new User(req.body);
+    try {
+        const savedUser = await user.save();
+        res.status(201).send(savedUser);
+    } catch (err) {
+        res.status(400).json({ message: 'One of the parameters is incorrect' });
+    }
+}
+
+async function login(req, res) {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        res.status(403).json({ message: 'One or more of the parameters are missing' });
+        return;
+    }
+    const userExist = await User.findOne({ username, password });
+    if (!userExist) {
+        res.status(403).json({ message: 'One of the paramateres is incorrect' });
+        return;
+    }
+    const token = jwt.sign({ id: userExist._id }, 'shahar');
+
+    res.json({ token });
+}
 
 module.exports = {
-    create: async (req, res) => {
-        const user = new User(req.body);
-        try {
-            const savedUser = await user.save();
-            res.status(201).send(savedUser);
-        } catch (err) {
-            res.status(400).send(err);
-        }
-    }
+    create,
+    login
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./config/routes');
+const cors = require('cors');
 const app = express();
 const port = 4000;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
@@ -11,8 +13,8 @@ mongoose.connect('mongodb://localhost:27017/instagram')
     .then(listen)
     .catch(err => console.error(err));
 
-    function listen() {
-        app.listen(port, () => {
-            console.log(`App listening at http://localhost:${port}`)
-        });
-    }
+function listen() {
+    app.listen(port, () => {
+        console.log(`App listening at http://localhost:${port}`)
+    });
+}
