22677bbfe0e8ff361223a9486732df39477fd20c
Ran Malki <ran.malki2@gmail.com>
2021-11-22 19:58:28 +0200

    b

--- src/config/env/index.js
+++ src/config/env/index.js
@@ -2,7 +2,7 @@ const development = require('./development');
 const production = require('./production');
 
 let environment = development;
-if (process.env.NODE_EMV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     environment = production;
 }
 
